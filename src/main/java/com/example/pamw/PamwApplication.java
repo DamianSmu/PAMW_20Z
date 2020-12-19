04aca79ff296014c6dd0ef48bbdd98459d66724d
DamianSmu <smugorzewski.ds@gmail.com>
2020-12-20 00:11:23 +0100

    Init db, dockerfile

--- src/main/java/com/example/pamw/PamwApplication.java
+++ src/main/java/com/example/pamw/PamwApplication.java
@@ -1,9 +1,18 @@
 package com.example.pamw;
 
+import com.example.pamw.entity.User;
+import com.example.pamw.repository.UserRepository;
+import com.example.pamw.security.RoleEnum;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class PamwApplication extends SpringBootServletInitializer {
@@ -15,5 +24,27 @@ public class PamwApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(PamwApplication.class);
+    }
+
+    @Bean
+    CommandLineRunner initDatabase(UserRepository userRepository, PasswordEncoder encoder) {
+
+        return args -> {
+            if(!userRepository.findByUsername("kurier").isPresent()){
+                User user = new User(
+                        "Imiekuriera",
+                        "Nazwiskokuriera",
+                        "kurier",
+                        "email@email.com",
+                        encoder.encode("kurier"),
+                        "adres"
+                );
+                Set<RoleEnum> roles = new HashSet<>();
+                roles.add(RoleEnum.COURIER);
+                roles.add(RoleEnum.USER);
+                user.setRoles(roles);
+                userRepository.save(user);
+            }
+        };
     }
 }
