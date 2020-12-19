60273dca546c3da3b9ca11cfb5e32ed6d968ad54
DamianSmu <smugorzewski.ds@gmail.com>
2020-12-19 18:06:20 +0100

    Heroku deploy

--- src/main/java/com/example/pamw/configuration/WebConfiguration.java
+++ src/main/java/com/example/pamw/configuration/WebConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE").allowedOrigins("http://localhost:9090")
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE").allowedOrigins("https://pamw-damian-smugorzewski.herokuapp.com")
                 .allowedHeaders("*").allowCredentials(true);
     }
 }
